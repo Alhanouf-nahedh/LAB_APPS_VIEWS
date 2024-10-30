7e9f4b109c878559c4334614ae5cb2068c83ca30
Alhanouf <hnno3040@gmail.com>
2024-10-30 10:29:34 +0300

    done

--- cars/app_name/urls.py
+++ cars/app_name/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, about, generate_password  
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('H', home, name='home'),
     path('about/', about, name='about'),
     path('random-password/', generate_password, name='generate_password'),  
 ]
